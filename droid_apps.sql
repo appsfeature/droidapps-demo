33c44a108b98830952bfd02d187eed719feccff3
abhijitrao <abhijitrao33@gmail.com>
2022-01-20 14:32:28 +0530

    adding databse file

---
+++ droid_apps.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 20, 2022 at 09:55 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `droid_apps`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_category`
+--
+
+CREATE TABLE `table_category` (
+  `pkg_id` varchar(100) NOT NULL,
+  `cat_id` int(100) NOT NULL,
+  `sub_cat_id` int(100) NOT NULL DEFAULT 0,
+  `cat_name` varchar(1000) NOT NULL,
+  `cat_type` int(100) NOT NULL DEFAULT 0,
+  `image` varchar(1000) DEFAULT NULL,
+  `order_id` int(100) NOT NULL DEFAULT 0,
+  `visibility` int(100) NOT NULL DEFAULT 1,
+  `json_data` varchar(5000) DEFAULT NULL,
+  `other_property` varchar(1000) DEFAULT NULL,
+  `updated_at` varchar(100) NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `table_category`
+--
+
+INSERT INTO `table_category` (`pkg_id`, `cat_id`, `sub_cat_id`, `cat_name`, `cat_type`, `image`, `order_id`, `visibility`, `json_data`, `other_property`, `updated_at`) VALUES
+('package id1', 13, 56, '0hgfgh', 0, NULL, 0, 1, NULL, NULL, '2022-01-20 12:58:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_content`
+--
+
+CREATE TABLE `table_content` (
+  `pkg_id` varchar(100) NOT NULL,
+  `id` int(100) NOT NULL,
+  `cat_id` int(100) NOT NULL DEFAULT 0,
+  `sub_cat_id` int(100) NOT NULL DEFAULT 0,
+  `title` varchar(1000) NOT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  `link` varchar(1000) DEFAULT NULL,
+  `visibility` int(100) NOT NULL DEFAULT 1,
+  `json_data` varchar(5000) DEFAULT NULL,
+  `other_property` varchar(1000) DEFAULT NULL,
+  `updated_at` varchar(100) NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `table_content`
+--
+
+INSERT INTO `table_content` (`pkg_id`, `id`, `cat_id`, `sub_cat_id`, `title`, `description`, `image`, `link`, `visibility`, `json_data`, `other_property`, `updated_at`) VALUES
+('package id1', 3, 13, 67, 'happy', NULL, NULL, NULL, 1, NULL, NULL, '2022-01-20 12:59:38');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `table_category`
+--
+ALTER TABLE `table_category`
+  ADD PRIMARY KEY (`cat_id`),
+  ADD UNIQUE KEY `cat_id` (`cat_id`);
+
+--
+-- Indexes for table `table_content`
+--
+ALTER TABLE `table_content`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `table_category`
+--
+ALTER TABLE `table_category`
+  MODIFY `cat_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `table_content`
+--
+ALTER TABLE `table_content`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
